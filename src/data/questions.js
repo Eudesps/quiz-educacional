38c143d2e74fcfc49124b1dca619ba16aa0eaad6
José Eudes <jeudes894@gmail.com>
2025-05-13 19:28:43 -0300

    Adicionando as perguntas e respostas

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,16 +1,285 @@
 const questions = [
-    {
-      question: "1. Qual é a capital do Brasil?",
-      options: ["A) Rio de Janeiro", "B) São Paulo", "C) Brasília", "D) Salvador", "E) Belo Horizonte"],
-      correct: 2
-    },
-    {
-      question: "2. Quantos planetas existem em nosso sistema solar?",
-      options: ["A) 7", "B) 8", "C) 9", "D) 10", "E) 6"],
-      correct: 1
-    }
-    // Adicione mais perguntas aqui
-  ];
+  {
+    question: "1. Este tipo é um exemplo de empreendedorismo que envolve a criação de empresas com base na colaboração de pessoas ou grupos.",
+    options: [
+      "A) Empreendedorismo Cooperativo", 
+      "B) Empreendedorismo Individual", 
+      "C) Empreendedorismo Informal", 
+      "D) Empreendedorismo Social"
+    ],
+    correct: 0
+  },
+  {
+    question: "2. Este tipo de empreendedorismo é uma pessoa jurídica criada pela lei complementar 128 de 2008. Esta permite ao trabalhador por conta própria regularizar seu negócio.",
+    options: [
+      "A) Empreendedorismo por Franquias", 
+      "B) Empreendedorismo Cooperativo", 
+      "C) Empreendedorismo Corporativo", 
+      "D) Empreendedorismo Individual"
+    ],
+    correct: 3
+  },
+  {
+    question: "3. Este tipo de empreendedorismo busca resolver problemas da comunidade de forma coletiva.",
+    options: [
+      "A) Empreendedorismo Ambiental", 
+      "B) Empreendedorismo Social", 
+      "C) Empreendedorismo Cooperativo", 
+      "D) Empreendedorismo Digital"
+    ],
+    correct: 1
+  },
+  {
+    question: "4. Este tipo de empreendedorismo busca a criatividade e a inovação dentro das empresas.",
+    options: [
+      "A) Empreendedorismo Cooperativo", 
+      "B) Empreendedorismo Individual", 
+      "C) Empreendedorismo Informal", 
+      "D) Empreendedorismo Corporativo ou Intraempreendedorismo"
+    ],
+    correct: 3
+  },
+  {
+    question: "5. Este tipo de empreendedorismo busca adotar práticas sustentáveis como o uso de energia renovável, redução de desperdícios e reciclagem.",
+    options: [
+      "A) Empreendedorismo Social", 
+      "B) Empreendedorismo por Franquias", 
+      "C) Empreendedorismo Ambiental ou Verde",
+      "D) Empreendedorismo Individual"
+    ],
+    correct: 2
+  },
+  {
+    question: "6. Fenômeno que surgiu como resultado de ativos técnicos, como a internet e as Tecnologias de Informação e Comunicação (TICs).",
+    options: [
+      "A) Empreendedorismo por Franquias", 
+      "B) Empreendedorismo Digital", 
+      "C) Empreendedorismo Social", 
+      "D) Empreendedorismo Corporativo"
+    ],
+    correct: 1
+  },
+  {
+    question: "7. Esse tipo de empreendedorismo é uma importante iniciativa da classe em busca da independência.",
+    options: [
+      "A) Empreendedorismo Digital", 
+      "B) Empreendedorismo Informal", 
+      "C) Empreendedorismo Individual", 
+      "D) Empreendedorismo Feminino"
+    ],
+    correct: 3
+  },
+  {
+    question: "8. Esse tipo de empreendedorismo envolve o modelo em que existe o franqueador e o franqueado.",
+    options: [
+      "A) Empreendedorismo Cooperativo",
+      "B) Empreendedorismo por Franquias", 
+      "C) Empreendedorismo Social", 
+      "D) Empreendedorismo Corporativo"
+    ],
+    correct: 1
+  },
+  {
+    question: "9. Este tipo de empreendedorismo envolve a criação de negócios sem registro legal ou regulamentação formal.",
+    options: [
+      "A) Empreendedorismo Informal", 
+      "B) Empreendedorismo Cooperativo", 
+      "C) Empreendedorismo Digital", 
+      "D) Empreendedorismo Social"
+    ],
+    correct: 0
+  },
+  {
+    question: "10. Este tipo de empreendedorismo a pessoa pode se registrar como Microempreendedor Individual (MEI) e Empresa Individual de Responsabilidade Limitada (EIRELI).",
+    options: [
+      "A) Empreendedorismo Corporativo", 
+      "B) Empreendedorismo Social", 
+      "C) Empreendedorismo Individual", 
+      "D) Empreendedorismo Digital"
+    ],
+    correct: 2
+  },
+  {
+    question: "11. Este tipo de empreendedorismo busca fortalecer o desenvolvimento comunitário ao gerar emprego, distribuir renda de forma equitativa e incentiva a colaboração entre os membros comunitários.",
+    options: [
+      "A) Empreendedorismo Corporativo", 
+      "B) Empreendedorismo Informal", 
+      "C) Empreendedorismo por Franquias", 
+      "D) Empreendedorismo Cooperativo"
+    ],
+    correct: 3
+  },
+  {
+    question: "12. Este tipo de empreendedorismo contribui para a geração de renda, cria oportunidades de trabalho e fortalece a economia local, embora careça de apoio formal e legalização.",
+    options: [
+      "A) Empreendedorismo Ambiental", 
+      "B) Empreendedorismo Informal", 
+      "C) Empreendedorismo Cooperativo", 
+      "D) Empreendedorismo Corporativo"
+    ],
+    correct: 1
+  },
+  {
+    question: "13. Este tipo de empreendedorismo busca acesso a financiamentos, conciliação entre vida profissional e pessoal, preconceito de gênero e menor representatividade em cargos de liderança.",
+    options: [
+      "A) Empreendedorismo Individual", 
+      "B) Empreendedorismo Informal", 
+      "C) Empreendedorismo Feminino", 
+      "D) Empreendedorismo Social"
+    ],
+    correct: 2
+  },
+  {
+    question: "14. Este tipo de empreendedorismo promove mudanças sociais positivas como, como a inclusão, a educação e a melhoria da qualidade de vida, ao criar soluções para problemas locais.",
+    options: [
+      "A) Empreendedorismo Social", 
+      "B) Empreendedorismo Ambiental", 
+      "C) Empreendedorismo Digital", 
+      "D) Empreendedorismo Individual"
+    ],
+    correct: 0
+  },
+  {
+    question: "15. Este tipo de empreendedorismo envolve oportunidades de novos negócios a partir de inovações ecológicas.",
+    options: [
+      "A) Empreendedorismo Social", 
+      "B) Empreendedorismo Digital", 
+      "C) Empreendedorismo Cooperativo", 
+      "D) Empreendedorismo Ambiental ou Verde"
+    ],
+    correct: 3
+  },
+  {
+    question: "16. Esse tipo de empreendedorismo não está limitado ao e-commerce ou e-business, seu impacto vai além desse modelo de negócios e engloba a mudança de produtos físicos em serviços digitais.",
+    options: [
+      "A) Empreendedorismo Informal", 
+      "B) Empreendedorismo Digital", 
+      "C) Empreendedorismo Social", 
+      "D) Empreendedorismo Individual"
+    ],
+    correct: 1
+  },
+  {
+    question: "17. Este tipo de empreendedorismo inclui a distribuição de produtos e serviços, mediante condições definidas em contrato, entre franqueador e franqueado.",
+    options: [
+      "A) Empreendedorismo Digital", 
+      "B) Empreendedorismo Informal", 
+      "C) Empreendedorismo por Franquias", 
+      "D) Empreendedorismo Cooperativo"
+    ],
+    correct: 2
+  },
+  {
+    question: "18. Este tipo de empreendedorismo envolve pessoas que trabalham por conta própria.",
+    options: [
+      "A) Empreendedorismo Informal", 
+      "B) Empreendedorismo Individual", 
+      "C) Empreendedorismo por Franquias", 
+      "D) Empreendedorismo Cooperativo"
+    ],
+    correct: 1
+  },
+  {
+    question: "19. Este tipo de empreendedorismo busca oferecer recursos para inovação, treinamento, programas de reconhecimento e criação de uma cultura de valorização de novas ideias em uma organização.",
+    options: [
+      "A) Empreendedorismo Corporativo ou Intraempreendedorismo",
+      "B) Empreendedorismo Cooperativo",
+      "C) Empreendedorismo Informal",
+      "D) Empreendedorismo Individual"
+    ],
+    correct: 0
+  },
+  {
+    question: "20. Esse tipo de empreendedorismo busca melhorar a sociedade por meio de projetos que focam no crescimento humano e social, deixando os lucros em segundo plano.",
+    options: [
+      "A) Empreendedorismo Feminino",
+      "B) Empreendedorismo Social",
+      "C) Empreendedorismo Individual",
+      "D) Empreendedorismo Corporativo"
+    ],
+    correct: 1
+  },
+  {
+    question: "21. Este tipo de empreendedorismo identifica oportunidades comerciais por meio da utilização de recursos digitais. Possui uma busca constante por inovações, com riscos calculados, transferindo uma obtenção de renda, reconhecimento e expansão no mercado.",
+    options: [
+      "A) Empreendedorismo Corporativo",
+      "B) Empreendedorismo Social",
+      "C) Empreendedorismo Informal",
+      "D) Empreendedorismo Digital"
+    ],
+    correct: 3
+  },
+  {
+    question: "22. Este tipo de empreendedorismo busca resolver um problema social que o Estado não consegue atender de modo satisfatório, mesmo que uma instituição não produza lucros.",
+    options: [
+      "A) Empreendedorismo Social",
+      "B) Empreendedorismo Digital",
+      "C) Empreendedorismo Ambiental",
+      "D) Empreendedorismo Cooperativo"
+    ],
+    correct: 0
+  },
+  {
+    question: "23. Este tipo de empreendedorismo visa manter a sustentabilidade ambiental e financeira da instituição, independentemente de seu aspecto teoricamente filantrópico.",
+    options: [
+      "A) Empreendedorismo Social",
+      "B) Empreendedorismo Corporativo",
+      "C) Empreendedorismo Ambiental ou Verde",
+      "D) Empreendedorismo Informal"
+    ],
+    correct: 2
+  },
+  {
+    question: "24. Este tipo de empreendedorismo refere-se a empreendimento criado por mulheres.",
+    options: [
+      "A) Empreendedorismo Individual",
+      "B) Empreendedorismo Feminino",
+      "C) Empreendedorismo Corporativo",
+      "D) Empreendedorismo por Franquia"
+    ],
+    correct: 1
+  },
+  {
+    question: "25. Este tipo de empreendedorismo aplica princípios empreendedores (como inovação e criatividade) dentro de organizações já estabelecidas, visando melhorar processos internos e gerar inovações no ambiente empresarial.",
+    options: [
+      "A) Empreendedorismo Cooperativo",
+      "B) Empreendedorismo Digital",
+      "C) Empreendedorismo Social",
+      "D) Empreendedorismo Corporativo ou Intraempreendedor"
+    ],
+    correct: 3
+  },
+  {
+    question: "26. Este tipo de empreendedorismo leva em conta dois aspectos fundamentais: o social, com origem em iniciativas cooperativas, e o econômico, com o objetivo de preencher lacunas deixadas pelo capital. Trata-se do empreendedorismo cooperativo.",
+    options: [
+      "A) Empreendedorismo Corporativo",
+      "B) Empreendedorismo Informal",
+      "C) Empreendedorismo Cooperativo",
+      "D) Empreendedorismo Individual"
+    ],
+    correct: 2
+  },
+  {
+    question: "27. Este tipo de empreendedorismo é aquele que atua sem CNPJ ou nota fiscal, sendo bastante executado por brasileiros que sonham ser donos da própria empresa.",
+    options: [
+      "A) Empreendedorismo Social",
+      "B) Empreendedorismo Informal",
+      "C) Empreendedorismo Corporativo",
+      "D) Empreendedorismo Feminino"
+    ],
+    correct: 1
+  },
+  {
+    question: "28. Este tipo de empreendedorismo envolve modelos de negócios que, em tese, são mais fáceis de serem executados, pois o empreendedor já recebe uma empresa com processos claros e definidos, como as franquias.",
+    options: [
+      "A) Empreendedorismo Cooperativo",
+      "B) Empreendedorismo Corporativo",
+      "C) Empreendedorismo por Franquias",
+      "D) Empreendedorismo Digital"
+    ],
+    correct: 2
+  }
+];
   
   export default questions;
   
